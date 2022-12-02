a847222c23adce3a366cc6b44d95359dd93c4a09
earl starks <earlstarks@rocketmail.com>
2022-12-01 19:03:15 -0600

    completed

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool Isdriveable { get; set; }
+
+        public void MakeEngineNoise()
+        {
+        Console.WriteLine("Vroom");
+        }
+
+        public void MakeHonkNoise ()
+        {
+            Console.WriteLine("Honk-Honk!");
+        }
+       
+       
+    }
+
+    public class CarLot { }
+    public string ListOfCars { get; set; }
+
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 
 namespace CarLotSimulator
 {
@@ -22,12 +23,77 @@ namespace CarLotSimulator
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
 
+
+            Console.WriteLine("First Car");
+            Console.WriteLine(" I will use STANDARD MEMBER INITIALIZATION BELOW!");
+
+            Car myCar = new Car();
+            myCar.Year = 2000;
+            myCar.Make = "Chevy";
+            myCar.Model = "Cruise";
+            myCar.Isdriveable = false;
+
+            myCar.MakeEngineNoise();
+            myCar.MakeHonkNoise();
+
+            Console.WriteLine($" {myCar.Year} {myCar.Make} {myCar.Model} {myCar.Isdriveable}");
+
+
+            Console.WriteLine("Second Car");
+            Console.WriteLine(" I will use SMI and Object Initializer Syntax Below!");
+
+            Car myCar2 = new Car();
+            myCar2.Year = 2005;
+            myCar2.Make = "Chevy";
+            myCar2.Model = "Tahoe";
+            myCar2.Isdriveable = false;
+
+            myCar2.MakeEngineNoise();
+            myCar2.MakeHonkNoise();
+
+            Console.WriteLine($" {myCar2.Year} {myCar2.Make} {myCar2.Model} {myCar2.Isdriveable}");
+
+            Car car2 = new Car() { Year = 2005, Make = "Chevy", Model = " Tahoe", Isdriveable = false };
+
+            Console.WriteLine("Third Car");
+            Console.WriteLine(" I will use SMI and Constructor Initializer Below!");
+
+            Car car3 = new Car(2010, "Chevy", "Malibu", Isdriveable);   
+
+            Car myCar3 = new Car();
+            myCar3.Year = 2010;
+            myCar3.Make = "Chevy";
+            myCar3.Model = "Malibu";
+            myCar3.Isdriveable = false;
+
+            myCar3.MakeEngineNoise();
+            myCar3.MakeHonkNoise();
+
+            Console.WriteLine($" {myCar3.Year} {myCar3.Make} {myCar3.Model} {myCar3.Isdriveable}");
+
+            Car myCar4 = new Car();
+            myCar4.Year = 2010;
+            myCar4.Make = "Chevy";
+            myCar4.Model = "Corvette";
+            myCar4.Isdriveable = false;
+
+            myCar4.MakeEngineNoise();
+            myCar4.MakeHonkNoise();
+
+            Console.WriteLine($" {myCar4.Year} {myCar4.Make} {myCar4.Model} {myCar4.Isdriveable}");
+
             //*************BONUS X 2*************//
 
             //Create a CarLot class
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+        
+        
+        
+        
         }
+
+
     }
 }
